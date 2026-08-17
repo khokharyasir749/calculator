250294e284309cf8ceafc6550543c245814ae7a6
Yasir Khokhar <khokharyasir749@gmail.com>
2026-08-17 00:37:15 -0700

    Initial calculator app

---
+++ script.js
@@ -0,0 +1,26 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let expression = display.value;
+
+        // Convert percentage
+        expression = expression.replace(/(\d+(\.\d+)?)%/g, "($1/100)");
+
+        display.value = eval(expression);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
